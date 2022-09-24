d87dd38776dd6e841b198fedcbc41391322c921d
F3B2d <umdcja@gmail.com>
2022-09-24 20:27:53 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "LHVr9yVkcyBigUaGD6kD1X0baHPupQpP"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "LHVr9yVkcyBigUaGD6kD1X0baHPupQpPs"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
